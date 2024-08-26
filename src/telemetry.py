83f6324b58306186cf4ce8fdef3bd2520a6d18bf
Oleksandr <sasha.tretyak@gmail.com>
2024-08-26 18:24:28 +0100

    2

--- src/telemetry.py
+++ src/telemetry.py
@@ -1,5 +1,5 @@
 from flask import request
-from opentelemetry import trace
+from opentelemetry import trace, metrics
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.trace import TracerProvider, Span
@@ -17,7 +17,9 @@ from requests import PreparedRequest
 import uuid
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 # from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 
 
 def configure_opentelemetry(app):
@@ -41,17 +43,23 @@ def configure_opentelemetry(app):
 
     set_global_textmap(TraceContextTextMapPropagator())
 
+    resource = Resource.create({SERVICE_NAME: "tracer"})
     # OpenTelemetry Configuration
     trace.set_tracer_provider(
-        TracerProvider(resource=Resource.create({SERVICE_NAME: "tracer"}))
+        TracerProvider(resource=resource)
     )
     tracer = trace.get_tracer(__name__)
-
     # Configure OpenTelemetry to export traces to an OTLP collector
     main_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
     # main_exporter = OTLPSpanExporter(endpoint="http://localhost:4318")
     batch_span_processor = BatchSpanProcessor(main_exporter)
     trace.get_tracer_provider().add_span_processor(batch_span_processor)
+
+    # Configure OpenTelemetry metrics
+    metrics_exporter = OTLPMetricExporter(endpoint="http://localhost:4317", insecure=True)
+    metric_reader = PeriodicExportingMetricReader(exporter=metrics_exporter, export_interval_millis=1000)
+    meter_provider=MeterProvider(resource=resource, metric_readers=[metric_reader])
+    metrics.set_meter_provider(meter_provider)
 
     # Add a SimpleSpanProcessor with a ConsoleSpanExporter to log spans to the console
     console_exporter = ConsoleSpanExporter()
